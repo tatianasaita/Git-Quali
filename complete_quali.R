0e474e752151f15d55e4781bd05f7fabfd7b146c
Tatiana Mari Saita <143027974+tatianasaita@users.noreply.github.com>
2025-06-29 09:11:54 -0300

    Add files via upload
    complete method

---
+++ complete_quali.R
@@ -0,0 +1,37 @@
+
+setwd("E:\\Download\\Tatiana-Scriba\\Git-Quali-master\\Git-Quali-master")
+
+# Sequencia-Grafo-Matriz de adjacência
+source("generateCombinations.R")
+
+source("createAdjMatrix.R")
+
+source("createNet.R")
+
+source("sequence-graph-adjmatrices1.R")
+
+
+# Análise de distâncias e dendrograma
+source("dist_matrix_dendrogram.R")
+
+# Clusterização
+source("clusterization2.R")
+
+source("adaptive_height_clustering.R")
+
+source("assign_subclusters.R")
+
+source("dynamic_subclustering.R")
+
+source("plot_dendrogram_clusters.R")
+
+source("automatic_clusterization.R")
+
+source("create_subclusters.R")
+
+# Seleção de características e classificação
+source("feature_selecion3.R")
+
+source("classification4.R")
+
+
